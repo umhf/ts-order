331f25a203c6a13be5941d4697b8e63b26f29938
Uli Schmid <tech@trbo.com>
2022-12-21 11:21:07 +0100

    finish site

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			white: colors.slate,
+			colored: colors.sky
+		  },
+	},
+	plugins: [],
+}
