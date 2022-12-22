70a8b0c046aaccf21c27fa2bdb0b47ef44f873b0
Uli Schmid <tech@trbo.com>
2022-12-22 07:55:59 +0100

    fix hook url for function

--- netlify/functions/build.js
+++ netlify/functions/build.js
@@ -3,7 +3,7 @@ import { schedule } from '@netlify/functions'
 
 // This is a sample build hook URL
 const BUILD_HOOK =
-  'curl -X POST -d {} https://api.netlify.com/build_hooks/63a2ddb7e4e1fa618caa335e'
+  'https://api.netlify.com/build_hooks/63a2ddb7e4e1fa618caa335e'
 
 const handler = schedule('0 0 * * 1-5', async () => {
   await fetch(BUILD_HOOK, {
