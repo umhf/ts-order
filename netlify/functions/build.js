998ba1d0069f823fdf0b6bbcdf450176c652a20a
Uli Schmid <tech@trbo.com>
2022-12-23 11:21:17 +0100

    add netlify.toml

--- netlify/functions/build.js
+++ netlify/functions/build.js
@@ -1,12 +1,11 @@
 import fetch from 'node-fetch'
 import fetch from "node-fetch"
-/* import { schedule } from '@netlify/functions' */
+import { schedule } from '@netlify/functions'
 
 const BUILD_HOOK =
   'https://api.netlify.com/build_hooks/63a2ddb7e4e1fa618caa335e'
 
-/* export const handler = schedule('5/5 * * * 1-5', async () => { */
-export const handler =  async () => {
+export const handler = schedule('5/5 * * * 1-5', async () => {
   await fetch(BUILD_HOOK, {
     method: 'POST'
   }).then(response => {
@@ -16,4 +15,4 @@ export const handler =  async () => {
   return {
     statusCode: 200
   }
-}
+})
