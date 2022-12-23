b9d187fdd2aa807ece85ec833b0f42265e2c4789
Uli Schmid <tech@trbo.com>
2022-12-23 11:45:46 +0100

    change cron schedule expression

--- netlify/functions/build.js
+++ netlify/functions/build.js
@@ -4,8 +4,7 @@ import { schedule } from '@netlify/functions'
 const BUILD_HOOK =
   'https://api.netlify.com/build_hooks/63a2ddb7e4e1fa618caa335e'
 
-export const handler = schedule('5/5 * * * 1-5', async () => {
-/* export const handler = async () => { */
+export const handler = schedule('0 0 * * 1-5', async () => {
   await fetch(BUILD_HOOK, {
     method: 'POST'
   }).then(response => {
