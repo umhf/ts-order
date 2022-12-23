665623d0b01ef76d9217c3fdb5ce083e2093084c
Uli Schmid <tech@trbo.com>
2022-12-23 11:39:31 +0100

    test

--- netlify/functions/testinger.js
+++ netlify/functions/testinger.js
@@ -1,11 +1,11 @@
 import fetch from 'node-fetch'
-import fetch from "node-fetch"
-import { schedule } from '@netlify/functions'
+/* import { schedule } from '@netlify/functions' */
 
 const BUILD_HOOK =
   'https://api.netlify.com/build_hooks/63a2ddb7e4e1fa618caa335e'
 
-export const handler = schedule('5/5 * * * 1-5', async () => {
+/* export const handler = schedule('5/5 * * * 1-5', async () => { */
+export const handler = async () => {
   await fetch(BUILD_HOOK, {
     method: 'POST'
   }).then(response => {
@@ -15,4 +15,4 @@ export const handler = schedule('5/5 * * * 1-5', async () => {
   return {
     statusCode: 200
   }
-})
+}
